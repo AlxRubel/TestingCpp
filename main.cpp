850221507c1c5ed569abab72ceccb972d189a984
AlxRubel <sasha.rubel.2002@gmail.com>
2021-08-10 02:19:58 +0300

    second message

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main ()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Super" << std::endl;
+    std::cout << "Super" << std::endl;
+
+    return 0;
+}
